c2cbdbb85bd47b04b7568005879061c16a2fabe4
Ella Mae Freshwater <emfresh2o@Ellas-MacBook-Pro.local>
2020-09-26 15:32:54 -0400

    tweak actions/reducers... trying remove/edit

--- smurfs/src/actions/actions.js
+++ smurfs/src/actions/actions.js
@@ -17,20 +17,56 @@ export const fetchSmurfs = () => (dispatch) => {
           })
   }
   
-  export const POST_DATA = "POST_DATA";
-  export const POST_SUCCESS = "POST_SUCCESS";
-  export const POST_FAIL = "POST_FAIL";
+export const POST_DATA = "POST_DATA";
+export const POST_SUCCESS = "POST_SUCCESS";
+export const POST_FAIL = "POST_FAIL";
   
-  export const postSmurfs = (smurfs) => (dispatch) => {
-      dispatch({ type: POST_DATA})
-        axios
-            .post("http://localhost:3333/smurfs", smurfs)
-            .then(res => {
-                console.log("Post success", res.data);
-                dispatch({type: POST_SUCCESS, payload: res.data})
-            })
-            .catch(err => {
-                console.log("Error:", err);
-                dispatch({type: POST_FAIL, payload: err.response})
-            })
-    }
+export const postSmurfs = (smurfs) => (dispatch) => {
+    dispatch({ type: POST_DATA})
+      axios
+        .post("http://localhost:3333/smurfs", smurfs)
+        .then(res => {
+            console.log("Post success", res.data);
+            dispatch({type: POST_SUCCESS, payload: res.data})
+        })
+        .catch(err => {
+            console.log("Error:", err);
+            dispatch({type: POST_FAIL, payload: err.response})
+        })
+}
+
+export const DELETE_DATA = "DELETE_DATA";
+export const DELETE_SUCCESS = "DELETE_SUCCESS";
+export const DELETE_FAIL = "DELETE_FAIL";
+
+export const deleteSmurfs = (smurfs) => (dispatch) => {
+    dispatch({ type: DELETE_DATA})
+      axios
+        .delete("http://localhost:3333/smurfs/${smurfs.id}")
+        .then(res => {
+            console.log("Delete success", res.data);
+            dispatch({type: DELETE_SUCCESS, payload: res.data})
+        })
+        .catch(err => {
+            console.log("Error:", err);
+            dispatch({type: DELETE_FAIL, payload: err.response})
+        })
+}
+
+export const UPDATE_DATA = "UPDATE_DATA";
+export const UPDATE_SUCCESS = "UPDATE_SUCCESS";
+export const UPDATE_FAIL = "UPDATE_FAIL";
+
+export const updateSmurfs = (newSmurfs) => (dispatch) => {
+    dispatch({ type:UPDATE_DATA})
+      axios
+        .delete("http://localhost:3333/smurfs/${newSmurfs.id}", newSmurfs)
+        .then(res => {
+            console.log("Delete success", res.data);
+            dispatch({type: UPDATE_SUCCESS, payload: res.data})
+        })
+        .catch(err => {
+            console.log("Error:", err);
+            dispatch({type: UPDATE_FAIL, payload: err.response})
+        })
+}
--- smurfs/src/reducers/SmurfsReducer.js
+++ smurfs/src/reducers/SmurfsReducer.js
@@ -1,8 +1,10 @@
-import { FETCH_DATA, FETCH_SUCCESS, FETCH_FAIL, POST_DATA, POST_SUCCESS, POST_FAIL } from "../actions/actions";
+import { FETCH_DATA, FETCH_SUCCESS, FETCH_FAIL, POST_DATA, POST_SUCCESS, POST_FAIL, DELETE_DATA, DELETE_SUCCESS, DELETE_FAIL, UPDATE_DATA, UPDATE_SUCCESS, UPDATE_FAIL } from "../actions/actions";
   const initialState ={
     smurfs: [],
     isFetching: false,
     isPosting: false,
+    isDeleting: false,
+    isUpdating: false,
     error: ""
 }
 
@@ -15,37 +17,73 @@ const smurfsReducer = (state=initialState, actions) => {
                 isFetching: true
                 
             }
-            case FETCH_SUCCESS:
-                return{
-                    ...state,
-                    isFetching: false,
-                    smurfs: actions.payload
-                }
-                case FETCH_FAIL:
-                    return{
-                        ...state,
-                        isFetching: false,
-                        error: actions.payload
-                    }
-                    case POST_DATA:
-                        return{
-                            ...state,
-                            isPosting: true,
-                            smurfs: [...state.smurfs]
+        case FETCH_SUCCESS:
+            return{
+                ...state,
+                isFetching: false,
+                smurfs: actions.payload
+            }
+        case FETCH_FAIL:
+            return{
+                ...state,
+                isFetching: false,
+                error: actions.payload
+            }
+        case POST_DATA:
+            return{
+                ...state,
+                isPosting: true,
+                smurfs: [...state.smurfs]
                             
-                        }
-                        case POST_SUCCESS:
-                            return{
-                                ...state,
-                                isPosting: false,
-                                smurfs:actions.payload
-                            }
-                            case POST_FAIL:
-                                return{
-                                    ...state,
-                                    isPosting: false,
-                                    error: actions.payload
-                                }
+            }
+        case POST_SUCCESS:
+            return{
+                ...state,
+                isPosting: false,
+                smurfs:actions.payload
+            }
+        case POST_FAIL:
+            return{
+                ...state,
+                isPosting: false,
+                error: actions.payload
+            }
+        case DELETE_DATA:
+            return {
+                ...state,
+                isDeleting: true,
+                smurfs: [...state.smurfs]
+            }
+        case DELETE_SUCCESS:
+            return {
+                ...state,
+                isDeleting: false,
+                smurfs:actions.payload
+            }
+        case DELETE_FAIL:
+            return {
+                ...state,
+                isDeleting: false,
+                error: actions.payload
+            }
+        case UPDATE_DATA:
+            return {
+                ...state,
+                isUpdate: true,
+                smurfs: [...state.smurfs]
+            }
+        case UPDATE_SUCCESS:
+            return {
+                ...state,
+                isUpdate: false,
+                smurfs:actions.payload
+            }
+        case UPDATE_FAIL:
+            return {
+                ...state,
+                isUpdate: false,
+                error: actions.payload
+            }
         default:
             return state
     }
